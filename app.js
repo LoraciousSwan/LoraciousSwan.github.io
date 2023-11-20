c1a2e3fc7bff3b41df154843813009f3b69ea9a9
Cake <109514575+Cakedadev@users.noreply.github.com>
2023-11-20 03:13:48 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+// OnClick --> ScrollY = top
+function scrollFunc() {
+  window.scrollTo(0, 0);
+}
+
+const navSlide = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  // CLICK EVENT ON HAMBURGER ICON
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-active");
+
+    // TOOGLING OF MENU ICON
+    hamburger.classList.toggle("toggle");
+  });
+};
+
+// Wait for DOMContentLoaded event before initializing AOS
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init();
+  navSlide();
+});
+
+
+
+
+
+
+
+// *******************************************************
+
+// // Loader
+// let loader = document.getElementById("loader");
+// setTimeout(function myFunc(){
+//     loader.style.display="none";
+// },4500)
+
+// // OnClick --> ScrollY = top
+// function scrollFunc(){
+//     window.scrollTo(0,0);
+// }
+
+// const navSlide = () => {
+//     const hamburger = document.querySelector('.hamburger');
+//     const navLinks = document.querySelector('.nav-links');
+
+
+//     // CLICK EVENT ON HAMBURGER ICON
+//     hamburger.addEventListener('click' , ()=>{
+//         navLinks.classList.toggle('nav-active');
+
+//     // TOOGLING OF MENU ICON
+//     hamburger.classList.toggle('toggle')
+
+//     });
+// }
+// navSlide();
